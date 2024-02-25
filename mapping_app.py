504aaef79c838d0808003c0c71685f7509e55207
VMOnGit <114856002+VMOnGit@users.noreply.github.com>
2024-02-25 22:39:37 +0530

    Update mapping_app.py

--- mapping_app.py
+++ mapping_app.py
@@ -6,20 +6,10 @@ import urllib.request
 import streamlit as st
 url = "IP-ADRESS-URL-OF-ESP-SERVER"  # ESP's url, ex: https://192.168.102/ (Esp serial prints it when connected to wifi)
 
-def get_data():
-	global data
 
-	n = urllib.request.urlopen(url).read() # get the raw html data in bytes (sends request and warn our esp8266)
-	n = n.decode("utf-8") # convert raw html bytes format to string :3
-	
-	data = n.split() 			#<optional> split datas we got. (if you programmed it to send more than one value) It splits them into seperate list elements.
-#	data = list(map(int, data)) #<optional> turn datas to integers, now all list elements are integers.
-  global lat = data[0]
-  global lon = data[1]
-# Example usage
 while True:
 	get_data()
-aoi = get_aoi(coordinates = [lat,lon], Macau", radius=1100, rectangular=False)
+aoi = get_aoi(coordinates = [9.505941623973825, 76.54946128618556], radius=1100, rectangular=False)
 df = get_osm_geometries(aoi=aoi)
 
 fig = Plot(
