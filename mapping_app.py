2818b4cb03a22a8f22fb10839b5a71d7e0d0307d
VMOnGit <114856002+VMOnGit@users.noreply.github.com>
2024-02-25 23:09:35 +0530

    Update mapping_app.py

--- mapping_app.py
+++ mapping_app.py
@@ -4,17 +4,75 @@ from prettymapp.plotting import Plot
 from prettymapp.settings import STYLES
 import urllib.request
 import streamlit as st
-url = "IP-ADRESS-URL-OF-ESP-SERVER"  # ESP's url, ex: https://192.168.102/ (Esp serial prints it when connected to wifi)
 
+from utils import (
+    st_get_osm_geometries,
+    st_plot_all,
+    get_colors_from_style,
+    gdf_to_bytesio_geojson,
+)
+from prettymapp.geo import GeoCodingError, get_aoi
+from prettymapp.settings import STYLES
 
-#while True:
-	#get_data()
-aoi = get_aoi(coordinates = [9.505941623973825, 76.54946128618556], radius=1100, rectangular=False)
-df = get_osm_geometries(aoi=aoi)
+st.set_page_config(
+    page_title="prettymapp", page_icon="🖼️", initial_sidebar_state="collapsed"
+)
+st.markdown("# Prettymapp")
 
-fig = Plot(
-    df=df,
-    aoi_bounds=aoi.bounds,
-    draw_settings=STYLES["Peach"]
-).plot_all()
+result_container = st.empty()
+with st.spinner("Creating map... (may take up to a minute)"):
+    rectangular = shape != "circle"
+    try:
+        aoi = get_aoi(coordinates = [9.505941623973825, 76.54946128618556], radius=radius, rectangular=rectangular)
+    except GeoCodingError as e:
+        st.error(f"ERROR: {str(e)}")
+        st.stop()
+    df = st_get_osm_geometries(aoi=aoi)
+    config = {
+        "aoi_bounds": aoi.bounds,
+        "draw_settings": draw_settings,
+        "name_on": name_on,
+        "name": address if custom_title == "" else custom_title,
+        "font_size": font_size,
+        "font_color": font_color,
+        "text_x": text_x,
+        "text_y": text_y,
+        "text_rotation": text_rotation,
+        "shape": shape,
+        "contour_width": contour_width,
+        "contour_color": contour_color,
+        "bg_shape": bg_shape,
+        "bg_buffer": bg_buffer,
+        "bg_color": bg_color,
+    }
+    fig = st_plot_all(_df=df, **config)
+    # result_container.write(html, unsafe_allow_html=True)
+    st.pyplot(fig, pad_inches=0, bbox_inches="tight", transparent=True, dpi=300)
+
+# svg_string = plt_to_svg(fig)
+# html = svg_to_html(svg_string)
+# st.write("")
+# fname = slugify(address)
+# img_format = st.selectbox("Download image as", ["svg", "png", "jpg"], index=0)
+# if img_format == "svg":
+#     data = svg_string
+# elif img_format == "png":
+#     import io
+#
+#     data = io.BytesIO()
+#     fig.savefig(data, pad_inches=0, bbox_inches="tight", transparent=True)
+# st.download_button(label="Download image", data=data, file_name=f"{fname}.{img_format}")
+
+st.markdown("</br>", unsafe_allow_html=True)
+st.markdown("</br>", unsafe_allow_html=True)
+ex1, ex2 = st.columns(2)
+
+with ex1.expander("Export geometries as GeoJSON"):
+    st.write(f"{df.shape[0]} geometries")
+    st.download_button(
+        label="Download",
+        data=gdf_to_bytesio_geojson(df),
+        file_name=f"prettymapp_{address[:10]}.geojson",
+        mime="application/geo+json",
+    )
 
